e865ecd6f800d8cb05ebe0384dd9f00c01cdde65
CupPink <mardenzy.swk@gmail.com>
2024-12-23 20:44:31 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,2 +1,2 @@
 setclipboard("https://discord.gg/KyfvX2HB3v")
-game.Players.LocalPlayer:kick("Old Script Down! Get New Script > discord.gg/KyfvX2HB3v")
+game.Players.LocalPlayer:Kick("Join Discord Rise For Get New Script\nhttps://discord.gg/KyfvX2HB3v")
